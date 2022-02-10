e0fee3f2e5fc282da4c7f0ba3c8f4ccaa83a8398
Jack andy <80754943+Jacksito98@users.noreply.github.com>
2022-02-10 04:31:32 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+apt-get update
+apt-get upgrade
+apt-get install nodejs
+apt-get install libwebp
+apt-get install mc
+apt-get install ffmpeg
+apt-get install wget
+apt-get install tesseract
+apt-get install nmap
+wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+npm install
+
+echo "Descarga Completada al 100%
+Creador:  Jacksito98
+Numero: +27638888888
+Instagram: Jack_paymon
+Youtube: https://www.youtube.com/c/Jacksito98
+Kuin Besame el pito, Teamo
+
+Escribe node npm start para iniciar el bot"
